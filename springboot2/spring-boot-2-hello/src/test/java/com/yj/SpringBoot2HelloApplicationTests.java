fa75803c921c52bd74cbf5834da341582eaad2dc
muyi-jian <2628168756@qq.com>
2023-11-15 10:21:26 +0800

    ti

---
+++ springboot2/spring-boot-2-hello/src/test/java/com/yj/SpringBoot2HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
